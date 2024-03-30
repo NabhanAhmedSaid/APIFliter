44b0e4471dccc0e3982559ff9fb4a2b063e38ab7
safcsp dev <dev@test.com>
2024-03-30 23:21:43 +0400

    Add project files.

---
+++ APIFliter/Fliter/MyLogging.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace APIFliter.Fliter
+{
+    public class MyLogging :Attribute, IActionFilter
+    {
+        private readonly string _callerName;
+
+        public MyLogging(string callerName)
+        {
+            _callerName = callerName;
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            Console.WriteLine($"Filter Executed before {_callerName}");
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            Console.WriteLine($"Filter Executed After {_callerName}");
+
+        }
+    }
+}
