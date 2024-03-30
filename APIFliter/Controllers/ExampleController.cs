fe27d62970839c34e62c793f5ba86f97f4836252
safcsp dev <dev@test.com>
2024-03-30 23:30:10 +0400

    Add

---
+++ APIFliter/Controllers/ExampleController.cs
@@ -0,0 +1,21 @@
+﻿using APIFliter.Fliter;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIFliter.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExampleController : ControllerBase
+    {
+        [HttpGet("Time")]
+        [MyLogging("Time")]
+        public DateTime GetDate() { return DateTime.Now; }
+        [HttpGet("Get")]
+        [MyLogging("Get")]
+        public string Get()
+        {
+            return "This is example API";
+        }
+    }
+}
